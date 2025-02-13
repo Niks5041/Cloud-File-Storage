eb7832d9da613b21da6c528866347bf49bef1480
Nikson <nik504441@yandex.ru>
2025-02-13 21:31:35 +0300

    1 версия приложения

---
+++ src/main/java/ru/anikson/cloudfilestorage/CloudFSApp.java
@@ -0,0 +1,11 @@
+package ru.anikson.cloudfilestorage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudFSApp {
+    public static void main(String[] args) {
+        SpringApplication.run(CloudFSApp.class);
+    }
+}
